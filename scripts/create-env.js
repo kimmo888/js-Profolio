3e696c8509993247fb21dc7b7e6abbd96d6fb6e9
Kastro888 <kasto777333@gmail.com>
2022-06-07 11:04:13 -0500

    ok

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,3 +1,3 @@
-const fs = require('fs');   //fs es un modulo de node, que permite trabajar con el sistema operativo según sea el caso
+const fs = require('fs');   //fs file system es un modulo de node, que permite trabajar con el sistema, crear guardar o enviar datos al sistema operativo según sea el caso
 
 fs.writeFileSync('./.env', 'API=${process.env.API}\n');  //con esto estamos creando un archivo .env y escribiendo en este las variables que hacen falta para el deploy, el archivo se crea en el servidor que es donde va a correr
